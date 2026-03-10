376ff9b0a90107a2d50da2e7a30bea79725ccbd4
vimalraj1112 <vimalraj07145@gmail.com>
2026-03-10 22:35:55 +0530

    sort

---
+++ Sorting.py/origin_merge_sort.py
@@ -0,0 +1,47 @@
+low=0
+
+
+
+def rec(a,low,high):
+    if low==high:
+        return
+    mid=(low+high)//2
+
+    rec(a,low,mid)
+    rec(a,mid+1,high)
+    mer(a,low,mid,high)
+    return a
+
+
+
+
+def mer(a,low,mid,high):
+    i=low
+    j=mid+1
+    temp=[]
+    while i<=mid and j<=high:
+        if a[i]>a[j]:
+            temp.append(a[j])
+            j+=1
+
+        else:
+            temp.append(a[i])
+            i+=1
+
+    while i<=mid:
+        temp.append(a[i])
+        i+=1
+
+    while j<=high:
+        temp.append(a[j])
+        j+=1
+    print(temp)    
+
+    for i in range(low,high+1):
+        a[i]=temp[i-low]
+
+
+
+print(rec([5,4,3,2,1],0,4))               
+
+
