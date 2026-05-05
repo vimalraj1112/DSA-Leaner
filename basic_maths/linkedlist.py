f9947c58c196bbab2037c9e0ed23992f3ab8d846
vimalraj1112 <vimalraj07145@gmail.com>
2026-05-05 22:05:34 +0530

    linkedlist

--- basic_maths/linkedlist.py
+++ basic_maths/linkedlist.py
@@ -8,7 +8,7 @@ class linkedlist():
     def __init__(self):
         self.head=None
 
-    def add_at_tail(self,data):
+    def addAtTail(self,data):
         new_node=Node(data) 
 
         if not self.head:
@@ -22,7 +22,7 @@ class linkedlist():
 
         current.next=new_node
 
-    def add_at_head(self,data):
+    def addAtHead(self,data):
         new_node=Node(data)
         if not self.head:
             self.head= new_node
@@ -30,17 +30,58 @@ class linkedlist():
         new_node.next=self.head
         self.head=new_node
 
+    def get(self,index):
+        if not self.head:
+            return -1
+        i=0
+        current=self.head
+        while current:
 
+            if i==index:
+                return current.data
+            current=current.next
+            i+=1
+        return -1
+    
+    def addAtIndex(self,index,data):
+        new_node=Node(data)
+        if not self.head:
+            if index==0:
+                self.head=new_node
+            return
+        
+        if index==0:
+            if self.head:
+                new_node.next=self.head
+                self.head=new_node
+
+            else:
+                self.head=new_node    
+        
+        current=self.head
+        i=0
+        while current:
+            if index==i+1:
+                new_node.next=current.next
+                current.next=new_node
+            current=current.next    
+            i+=1    
+
+
+                
+            
     def display(self):
         current=self.head
         while current:
             print(current.data,end=" -> ")
             current=current.next
-linkedlist=linkedlist()
-linkedlist.add_at_tail(1) 
-linkedlist.add_at_tail(2)
-linkedlist.add_at_tail(3)
-linkedlist.add_at_head(0)
 
+linkedlist=linkedlist()
+linkedlist.addAtTail(1) 
+linkedlist.addAtTail(2)
+linkedlist.addAtTail(3)
+linkedlist.addAtHead(0)
+print(linkedlist.get(7))
+print(linkedlist.addAtIndex(0,'new'))
 
 linkedlist.display()     
