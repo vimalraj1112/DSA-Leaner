4ddcbd6320dfc319219d7bf7d6f0fdb7df929e4c
vimalraj1112 <vimalraj07145@gmail.com>
2026-08-13 23:00:33 +0530

    leetcode

--- basic_maths/practice.py
+++ basic_maths/practice.py
@@ -1,20 +1,51 @@
-def ans():
+# def ans():
 
-    nums=[1,2,3,2,5]
-    ans=nums[0]
+#     nums=[1,2,3,2,5]
+#     ans=nums[0]
 
-    for i in range(1,len(nums)):
-        if nums[i]==nums[i-1]+1:
-            ans+=nums[i]
-        else:
-            break
+#     for i in range(1,len(nums)):
+#         if nums[i]==nums[i-1]+1:
+#             ans+=nums[i]
+#         else:
+#             break
 
-    while ans:
-        if ans in nums:
-            ans+=1
-        else:
-            return ans            
+#     while ans:
+#         if ans in nums:
+#             ans+=1
+#         else:
+#             return ans            
             
     
 
-print(ans())
+# print(ans())
+
+# def ans():
+#     v=[1,5,10,50,100,500,1000]
+#     s='IVXLCDM'
+#     num = 3749
+#     d={}
+
+#     for i in range(len(v)):
+#         d[v[i]]=s[i]
+#     print(len(str(num))+1)
+#     return d
+# print(ans())    
+
+def ans():
+    n=15
+
+    ans=[]
+
+    for i in range(1,n+1):
+        if i%3==0 and i%5==0:
+            ans.append('FizzBuzz')
+        if i%3==0:
+            ans.append('Fizz')
+        if i%5==0:
+            ans.append('Buzz')
+        if i%3!=0 and i%5!=0:
+            ans.append(str(i))
+
+    return ans
+
+print(ans())        
